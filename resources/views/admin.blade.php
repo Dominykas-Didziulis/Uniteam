8ef1c04f2a876c0041fb62e07992091fff54a1fc
Nedas Makarevičius <83786916+nedmak@users.noreply.github.com>
2022-04-07 11:47:33 +0300

    Admin frontend
    Apipavidalintas admino langas

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,59 +1,66 @@
-@extends('layouts.footer')
-@section('Footer')
-@endsection
-
-@extends('layouts.adminheader')
-
-<form action='registracija' method='get' style="margin: 10%">
-    <input type='hidden' name='staffId' id='staffId' value='$staffId'> 
-    <td class=''>
-      <input type='submit' name='submit' id='submit' value='Naujo naudotojo registracija' class='bg-white'>
-    </td>
-</form>
-<table style=""; border="1">
- 
-    <tr>
-      <th scope="col" class="">
-            ID
-      </th>
-      <th scope="col" class="">
-          VARDAS
-      </th>
-      <th scope="col" class="">
-          PAVARDĖ
-      </th>
-      <th scope="col" class="">
-          ELEKTRONINIS PAŠTAS
-      </th>
-      <th scope="col" class="">
-          Slapyvardis
-      </th>
-      <th scope="col" class="">
-        Rolė
-      </th>
-    </tr>
-
-    @foreach($users as $user)
-    <tr>
-    <th scope="col" class="">
-        {{ $user['id']}}
-    </th>
-    <th scope="col" class="">
-        {{ $user['name']}}
-    </th>
-    <th scope="col" class="">
-        {{ $user['subname']}}
-    </th>
-    <th scope="col" class="">
-        {{ $user['email']}}
-    </th>
-    <th scope="col" class="">
-        {{ $user['nickname']}}
-    </th>
-    <th scope="col" class="">
-        {{ $user['id']}}
-    </th>
-    </tr>
-
-    @endforeach
-</table>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UNITEAM</title>
+    <link href="{{ URL::asset('css/admin.css') }}" rel="stylesheet" type="text/css" >
+    @extends('layouts.adminheader')
+</head>
+<body>
+    <form action='registracija' method='get'>
+        <input type='hidden' name='staffId' id='staffId' value='$staffId'> 
+        <td class=''>
+          <input type='submit' name='submit' id='submit' value='Naujo naudotojo registracija' class='bg-white'>
+        </td>
+    </form>
+    <table class="Lentele" cellspacing="0">
+     
+        <tr>
+          <th scope="col" class="id">
+                Id
+          </th>
+          <th scope="col" class="">
+              Vardas
+          </th>
+          <th scope="col" class="">
+              Pavardė
+          </th>
+          <th scope="col" class="Pastas">
+              El. paštas
+          </th>
+          <th scope="col" class="">
+              Slapyvardis
+          </th>
+          <th scope="col" class="">
+            Rolė
+          </th>
+        </tr>
+    
+        @foreach($users as $user)
+        <tr>
+        <td scope="col" class="">
+            {{ $user['id']}}
+        </td>
+        <td scope="col" class="">
+            {{ $user['name']}}
+        </td>
+        <td scope="col" class="">
+            {{ $user['subname']}}
+        </td>
+        <td scope="col" class="">
+            {{ $user['email']}}
+        </td>
+        <td scope="col" class="">
+            {{ $user['nickname']}}
+        </td>
+        <td scope="col" class="">
+            {{ $user['id']}}
+        </td>
+        </tr>
+    
+        @endforeach
+    </table>
+</body>
+</html>
