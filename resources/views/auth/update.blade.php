1b7a5e29d3beb6225b24a0e1e3e3afd2cc3fe8aa
Nedas Makarevičius <83786916+nedmak@users.noreply.github.com>
2022-05-13 05:16:33 +0300

    TIkiuos viskas veikia

--- resources/views/auth/update.blade.php
+++ resources/views/auth/update.blade.php
@@ -8,112 +8,6 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
-    {{-- <link rel="stylesheet" href="{{ asset('css/home.css') }}"> --}}
-    <title>UNITEAM</title>
-
-    
-</head>
-<body>
-    {{-- Navbar --}}
-    <nav class="navbar navbar-expand-md navbar-dark sticky-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/"><img src="Images/TEAM.png" style="width: 255px; height: 109px;"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/">Pagrindinis</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/">Žaidimai</a>
-                    </li>
-                    @auth
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('posts') }}">Komandos</a>
-                        </li>
-                    @endauth
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('about') }}">Apie mus</a>
-                        </li>
-                    @endguest
-                    @auth
-                        @if (auth()->user()->ulevel == 1)
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('admin') }}">Narių sąrašas</a>
-                        </li>
-
-                       
-
-                        @endif
-                    @endauth
-                    @auth
-                    <form action="{{ route('user.update', auth()->id()) }}" method="get">
-                            @csrf
-                            <button type="submit" class="User">
-                                test
-                            </button>
-                        </form>
-
-
-                        <form action="{{ route('logout', auth()->user()->id) }}" method="post">
-                            @csrf
-                            <button type="submit" class="nav-link" style="background: none; border: 0;">ATSIJUNGTI</button>
-                        </form>
-                    @endauth
-
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}" ><img src="Images/add.png" alt="Add user" style="height: 20px; width: 20px;">Prisijungti</a>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-
-            <form action="{{ route('user.update', auth()->id())  }}" method="post">
-                @csrf
-                <div class="mb-4">
-                    <label for="name" class="sr-only">Name</label>
-                    <input type="text" name="name" id="name" placeholder="Vardas"
-                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror"
-                        value="{{ $user['name'] }}">
-                    @error('name')
-                        <div class="text-red-500 mt-2 text-sm">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div class="mb-4">
-                    <label for="subname" class="sr-only">Subname</label>
-                    <input type="text" name="subname" id="subname" placeholder="Pavardė"
-                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror"
-                        value="{{ $user['subname'] }}">
-                    @error('subname')
-                        <div class="text-red-500 mt-2 text-sm">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div class="mb-4">
-                    <label for="nickname" class="sr-only">Nickname</label>
-                    <input type="text" name="nickname" id="nickname" placeholder="Prisijungimo vardas"
-                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror"
-                        value="{{ $user['nickname'] }}">
-                    @error('nickname')
-                        <div class="text-red-500 mt-2 text-sm">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
 
     <link rel="stylesheet" href="{{ asset('css/update.css') }}">
     <title>UNITEAM</title>
@@ -170,16 +64,6 @@
                             <button type="submit" class="nav-link" style="background: none; border: 0;">ATSIJUNGTI</button>
                         </form>
                     @endauth
-
-                <div class="mb-4">
-                    <button type="submit"
-                        class="bg-red-50 text-white px-4 py-3 rounded 
-               font-medium w-full">Patvirtinti pakeitimus</button>
-                </div>
-
-            </form>
-
-    </body>
                     @guest
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}" ><img src="Images/add.png" alt="Add user" style="height: 20px; width: 20px;">Prisijungti</a>
