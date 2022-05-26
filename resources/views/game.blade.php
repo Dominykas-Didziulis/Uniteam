4df4a96186bdaf264b8f0a269302826985345433
einpet <78473929+einpet@users.noreply.github.com>
2022-05-26 17:17:55 +0300

    Update game.blade.php

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -25,11 +25,11 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
+                    <li class="nav-item">
                         <a class="nav-link" href="/">Pagrindinis</a>
                     </li>
                     @auth
-                    <li class="nav-item">
+                    <li class="nav-item active">
                         <a class="nav-link" href="{{ route('game') }}">Žaidimai</a>
                     </li>
 
