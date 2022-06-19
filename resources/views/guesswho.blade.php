825971872e804e3581bddd3b79b35b7eec5f901d
Dominykas <didziulis.dominykas@gmail.com>
2022-06-19 16:36:31 +0300

    headeriai ant zaidimu

--- resources/views/guesswho.blade.php
+++ resources/views/guesswho.blade.php
@@ -6,8 +6,91 @@
     <title>Unity WebGL Player | Quiz Demo</title>
     <link rel="shortcut icon" href="QTemplateData/favicon.ico">
     <link rel="stylesheet" href="QTemplateData/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    <link href="{{ URL::asset('css/about.css') }}" rel="stylesheet" type="text/css" >
   </head>
   <body>
+
+        {{-- Navbar --}}
+        <nav class="navbar navbar-expand-md navbar-dark sticky-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/"><img src="../Images/TEAM.png" style="width: 255px; height: 109px;"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/">Pagrindinis</a>
+                        </li>
+                        @auth
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('game') }}">Žaidimai</a>
+                        </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('team') }}">Komanda</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('posts') }}">Pokalbiai</a>
+                            </li>
+                        @endauth
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('about') }}">Apie mus</a>
+                            </li>
+                        @endguest
+                        @auth
+                            @if (auth()->user()->ulevel == 1)
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin') }}">Narių sąrašas</a>
+                            </li>
+
+
+                            @endif
+                        @endauth
+                        @auth
+                            {{-- <li class="nav-item">
+                                <a class="nav-link" href="profile" style="font-style: italic;">{{ auth()->user()->nickname }}</a>
+                            </li> --}}
+
+                            <li class="nav-item">
+                                <li class="dropdown">
+                                    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"  style="background: none; border: 0">{{ auth()->user()->nickname }}<span class="caret"></span></button>
+                                    <ul class="dropdown-menu" style="background-color: rgb(44, 49, 99)">
+                                        <li><a class="nav-link" href="profile">Profilis</a></li>
+                                        <li><a class="nav-link" href="{{ route('user.update', auth()->id()) }}">Informacijos keitimas</a></li>
+                                      </ul>
+                                </li>
+                            </li>
+                            {{-- <form action="{{ route('user.update', auth()->id()) }}" method="get">
+                                @csrf
+                                <button type="submit" class="User">
+                                    Profilis
+                                </button>
+                            </form> --}}
+
+                            {{-- <a class="nav-link" href="{{ route('user.update', auth()->id()) }}">{{ auth()->user()->nickname }}</a>; --}}
+
+                            <form action="{{ route('logout', auth()->user()->id) }}" method="post">
+                                @csrf
+                                <button type="submit" class="nav-link" style="background: none; border: 0;">ATSIJUNGTI</button>
+                            </form>
+                        @endauth
+
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}" ><img src="Images/add.png" alt="Add user" style="height: 20px; width: 20px;">Prisijungti</a>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
     <div id="unity-container" class="unity-desktop">
       <canvas id="unity-canvas" width=960 height=600></canvas>
       <div id="unity-loading-bar">
